a452971bf36b4bcd5a3d52faa5d69c76c8e6dfed
nog <nog.guitar@gmail.com>
2019-07-17 22:06:51 -0300

    Incluindo arquivos para visualização e primeiro controller - Htmls - CSS - Imagens

---
+++ demo-ajax/src/main/java/com/mballem/demoajax/controller/HomeController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mballem.demoajax.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author andersonnogueira
+ * @since Jul 17, 2019
+ */
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String init() {
+		return "promo-add";
+	}
+}
