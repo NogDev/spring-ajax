d8f30b42f0975608f7a4b6012fc1f23f3d675e18
nog <nog.guitar@gmail.com>
2019-07-18 17:26:11 -0300

    Usando loader para requisição de salvar a página
    - Oculta o form durante a execução do loader
    - Exibe o form após o sucesso

--- demo-ajax/src/main/resources/static/js/promo-add.js
+++ demo-ajax/src/main/resources/static/js/promo-add.js
@@ -18,6 +18,10 @@ $("#form-add-promo").submit(function(evt) {
 		method: "POST",
 		url: "/promocao/save",
 		data: promo,
+		beforeSend: function() {
+			$("#form-add-promo").hide();
+			$("#loader-form").addClass("loader").show();
+		},
 		success: function() {
 			//apaga conteudo de todos inputs
 			$("#form-add-promo").each(function() {
@@ -31,8 +35,14 @@ $("#form-add-promo").submit(function(evt) {
 		error: function(xhr) {
 			console.log("> error: ", xhr.responseText);
 			$("#alert").addClass("alert alert-danger").text("Não foi possível salvar esta promoção.")
+		},
+		complete: function() {
+			$("#loader-form").fadeOut(800, function() {
+				$("#form-add-promo").fadeIn(250); //retornar o formulario
+				$("#loader-form").removeClass("loader");
+			});
 		}
-	})
+	});
 	
 });
 
