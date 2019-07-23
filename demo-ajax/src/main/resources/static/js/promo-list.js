c6d2a7692ac1bb8f8dce2599e824616c5a70e838
nog <nog.guitar@gmail.com>
2019-07-23 11:53:15 -0300

    Exibir loader durante a requisição e o botão no final da lista

--- demo-ajax/src/main/resources/static/js/promo-list.js
+++ demo-ajax/src/main/resources/static/js/promo-list.js
@@ -1,4 +1,13 @@
 var pageNumber = 0;
+
+//ao iniciar a pagina
+$(document).ready(function() {
+	//ocultando esse itens (.hide)
+	$("#loader-img").hide();
+	$("#fim-btn").hide();
+});
+
+
 //efeito infinie scroll
 $(window).scroll(function() {
 	var scrollTop = $(this).scrollTop();
@@ -26,11 +35,28 @@ function loadByScroolBar(pageNumber) {
 		data: {
 			page: pageNumber
 		},
+		beforeSend: function() {
+			$("#loader-img").show();
+		},
 		success: function(response){
 			//console.log("resposta > ", response);
+			
+			if (response.length > 150) {
+				
+			
 			$(".row").fadeIn(250, function() {
 				$(this).append(response);
 			});
+			}else{
+				$("#fim-btn").show();
+				$("#loader-img").removeClass("loader");
+			}
+		},
+		error: function(xhr) {
+			alert("Ops, ocorreu um erro: " + xhr.status + " - " + xhr.statusText);
+		},
+		complete: function() {
+			$("#loader-img").hide();
 		}
 	})
 }
