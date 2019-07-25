73f1d9d1bedd2b3ac748302885bae117ac4c8be0
nog <nog.guitar@gmail.com>
2019-07-25 01:01:21 -0300

    Capturando os valores de id do botão de like

--- demo-ajax/src/main/resources/static/js/promo-list.js
+++ demo-ajax/src/main/resources/static/js/promo-list.js
@@ -60,3 +60,9 @@ function loadByScroolBar(pageNumber) {
 		}
 	})
 }
+
+//adcionar likes
+$("button[id*='likes-btn-']").on("click", function() {
+	var id = $(this).attr("id").split("-")[2];
+	console.log("id", id);
+});
