3d5d7cacf517029ce3b63104d0b9fbe6a5d7ee03
nog <nog.guitar@gmail.com>
2019-07-27 20:52:40 -0300

    Filtrando por titulo, categoria ou site

--- demo-ajax/src/main/java/com/mballem/demoajax/repository/PromocaoRepository.java
+++ demo-ajax/src/main/java/com/mballem/demoajax/repository/PromocaoRepository.java
@@ -21,6 +21,11 @@ import com.mballem.demoajax.domain.Promocao;
  */
 public interface PromocaoRepository extends JpaRepository<Promocao, Long>{
 	
+	@Query("select p from Promocao p where p.site like %:search% "
+			+ "or p.titulo like %:search% "
+			+ "or p.categoria.titulo like %:search%")
+	Page<Promocao> findByTituloOrSiteOrCategoria(@Param("search") String search, Pageable pageable);
+	
 	@Query("select p from Promocao p where p.site like :site")
 	Page<Promocao> findBySite(@Param("site") String site, Pageable pageable);
 	
--- demo-ajax/src/main/java/com/mballem/demoajax/service/PromocaoDataTablesService.java
+++ demo-ajax/src/main/java/com/mballem/demoajax/service/PromocaoDataTablesService.java
@@ -38,9 +38,11 @@ public class PromocaoDataTablesService {
 		
 		Sort.Direction direction = orderBy(request);
 		
+		String search = searchBy(request);
+		
 		Pageable pageable = PageRequest.of(current, length, direction, column);
 		
-		Page<Promocao> page = queryBy(repository, pageable);
+		Page<Promocao> page = queryBy(search, repository, pageable);
 		
 		Map<String, Object> json = new LinkedHashMap<>();
 		json.put("draw", draw);
@@ -52,13 +54,29 @@ public class PromocaoDataTablesService {
 
 	
 	/**
+	 * @param request
+	 */
+	private String searchBy(HttpServletRequest request) {
+		
+		return request.getParameter("search[value]").isEmpty()
+				? ""
+				:request.getParameter("search[value]");
+	}
+
+
+	/**
 	 * @param repository
 	 * @param pageable
 	 * @return
 	 */
-	private Page<Promocao> queryBy(PromocaoRepository repository, Pageable pageable) {
+	private Page<Promocao> queryBy(String search,PromocaoRepository repository, Pageable pageable) {
 		
-		return repository.findAll(pageable);
+		if (search.isEmpty()) {
+			
+			return repository.findAll(pageable);
+		}
+		
+		return repository.findByTituloOrSiteOrCategoria(search, pageable);
 	}
 
 
