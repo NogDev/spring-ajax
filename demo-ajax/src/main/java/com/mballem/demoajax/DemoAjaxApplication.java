c22efedfe37e5fb83f928e8454d60b52ec357c05
nog <nog.guitar@gmail.com>
2019-07-29 20:12:20 -0300

    Integrando a DWR ao Spring
    - Integrando a biblioteca que permite a utilização de ajax reverso com o
    Spring

--- demo-ajax/src/main/java/com/mballem/demoajax/DemoAjaxApplication.java
+++ demo-ajax/src/main/java/com/mballem/demoajax/DemoAjaxApplication.java
@@ -1,12 +1,16 @@
 package com.mballem.demoajax;
 
+import org.directwebremoting.spring.DwrSpringServlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
 import com.mballem.demoajax.service.SocialMetaTagService;
-
+@ImportResource(locations = "classpath:dwr-spring.xml")
 @SpringBootApplication
 public class DemoAjaxApplication implements CommandLineRunner{
 
@@ -23,5 +27,17 @@ public class DemoAjaxApplication implements CommandLineRunner{
 //		SocialMetaTag tag = service.getSocialMetatagByUrl("https://www.pichau.com.br/headset-logitech-g933-artemis-spectrum-7-1-surround-usb-wireless-981-000598");
 //		System.out.println(tag);
 
+	}
+	
+	@Bean
+	public ServletRegistrationBean<DwrSpringServlet> dwrSpringServlet() {
+		DwrSpringServlet dwrServlet = new DwrSpringServlet();
+		ServletRegistrationBean<DwrSpringServlet> registrationBean =
+				new ServletRegistrationBean<>(dwrServlet, "/dwr/*");
+		registrationBean.addInitParameter("debug", "true");
+		registrationBean.addInitParameter("activeReverseAjaxEnabled", "true");
+		
+		return registrationBean;
+		
 	}
 }
